26121e2dff9d3acb63f631103671cb6fefcf8c0e
Dennis Zimmermann <dennzimm@icloud.com>
2018-02-05 12:54:20 +0100

    Add index controller

---
+++ controllers/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.use('/todos', require('./todos'));
+router.use('/users', require('./users'));
+
+module.exports = router;
